8a473b1a871b1a1f145f35b3895e6e10acde7a30
Guillaume Piot <guillaume@cotidia.com>
2014-02-13 13:47:02 +0000

    Updated urls

--- registration_email/auth_urls.py
+++ registration_email/auth_urls.py
@@ -46,21 +46,21 @@ urlpatterns = patterns(
         name='auth_password_reset',
     ),
     url(
-        r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         auth_views.password_reset_confirm,
         {'template_name': 'registration/password_reset_confirm.html'},
-        name='auth_password_reset_confirm',
+        name='password_reset_confirm',
     ),
     url(
         r'^password/reset/complete/$',
         auth_views.password_reset_complete,
         {'template_name': 'registration/password_reset_complete.html'},  # NOQA
-        name='auth_password_reset_complete',
+        name='password_reset_complete',
     ),
     url(
         r'^password/reset/done/$',
         auth_views.password_reset_done,
         {'template_name': 'registration/password_reset_done.html'},
-        name='auth_password_reset_done',
+        name='password_reset_done',
     ),
 )
