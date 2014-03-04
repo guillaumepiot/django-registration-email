9f93de47cb1d5d8ec6ebf9421a6eff136a9b94a1
Guillaume Piot <guillaume@cotidia.com>
2014-03-04 09:06:55 +0000

    Fixed change password url

--- registration_email/auth_urls.py
+++ registration_email/auth_urls.py
@@ -37,7 +37,7 @@ urlpatterns = patterns(
         r'^password/change/done/$',
         auth_views.password_change_done,
         {'template_name': 'registration/password_change_done_custom.html'},
-        name='auth_password_change_done',
+        name='password_change_done',
     ),
     url(
         r'^password/reset/$',
